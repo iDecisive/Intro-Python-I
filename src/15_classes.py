d89d6ace84a0c8bc4642fabe40fc3943e0f98759
Daniel Johnson <danielaugustusjohnson@gmail.com>
2020-08-05 09:23:47 -0400

    fixed class mistake

--- src/15_classes.py
+++ src/15_classes.py
@@ -2,10 +2,15 @@
 # constructor
 
 # YOUR CODE HERE
+
 class LatLon:
     def __init__(self, lat, lon):
         self.lat = lat
         self.lon = lon
+
+cords = LatLon(41, 42)
+
+print(cords.__dict__)
 
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon. Look up the `super` method.
@@ -14,9 +19,14 @@ class LatLon:
 
 class Waypoint(LatLon):
     def __init__(self, name, lat, lon):
-        super().__init__(lat, lat)
-        super().__init__(lon, lon)
+        super().__init__(lat, lon)
         self.name = name
+    def __str__(self):
+        return f'Name: {self.name}, Lat: {self.lat}, Lon: {self.lon}'
+
+beach = Waypoint('beach', 41, 42)
+
+print(beach.__dict__)
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
@@ -25,17 +35,19 @@ class Waypoint(LatLon):
 
 class Geocache(Waypoint):
     def __init__(self, name, difficulty, size, lat, lon):
-        super().__init__(name, name)
-        super().__init__(lat, lat)
-        super().__init__(lon, lon)
-        self.size = size
+        super().__init__(name, lat, lon)
         self.difficulty = difficulty
+        self.size = size
+    def __str__(self):
+        return f'Name: {self.name}, Difficulty: {self.difficulty}, Size: {self.size}, Lat: {self.lat}, Lon: {self.lon}'
 
 # Make a new waypoint and print it out: "Catacombs", 41.70505, -121.51521
 
 # YOUR CODE HERE
 
 waypoint = Waypoint('Catacombs', 41.70505, -121.51521)
+
+print(f'"{waypoint.name}", {waypoint.lat}, {waypoint.lon}')
 
 # Without changing the following line, how can you make it print into something
 # more human-readable? Hint: Look up the `object.__str__` method
